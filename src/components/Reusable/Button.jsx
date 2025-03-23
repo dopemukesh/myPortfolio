e39e7d1841efe1cabf5d892f91392a32348a32f5
dopemukesh <dope.mukeshyadav@gmail.com>
2025-03-24 00:17:20 +0530

    some miner changes are made. like buttons and resume updated bl

--- src/components/Reusable/Button.jsx
+++ src/components/Reusable/Button.jsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+/* eslint-disable react/prop-types */
 const Button = ({
     children,
     onClick,
@@ -12,7 +11,7 @@ const Button = ({
     const variantClasses = {
         primary: 'bg-gradient-to-r from-brandBlue-500 to-brandBlue-600 text-white',
         secondary: 'bg-brandDark-800 dark:bg-brandDark-100 hover:bg-brandDark-700 dark:hover:bg-brandDark-300 text-white dark:text-brandDark-800 dark:hover:bg-brandDark-200',
-        outline: 'bg-transparent text-brandBlue-500 border border-brandBlue-500 hover:bg-brandBlue-500 hover:text-white',
+        outline: 'bg-transparent text-brandBlue-500 border-[1.5px] border-brandBlue-500 hover:border-brandDark-500 hover:text-brandDark-800 dark:hover:text-white',
         danger: 'bg-red-500 text-white hover:bg-red-600',
     };
 
@@ -21,7 +20,7 @@ const Button = ({
             type={type}
             onClick={onClick}
             disabled={disabled}
-            className={`inline-block px-5 py-2 text-sm rounded-md ${variantClasses[variant] || variantClasses.primary
+            className={`box-border inline-block px-7 py-3 font-medium rounded-xl active:scale-90 transition-all duration-500 ${variantClasses[variant] || variantClasses.primary
                 } ${disabled ? 'opacity-50 cursor-not-allowed' : ''
                 } ${className}`}
         >
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import resumePdfbl from '../assets/resume/mukeshyadav-frontend-dev-resume-bl.pdf';
 import resumePdfbw from '../assets/resume/mukeshyadav-frontend-dev-resume-bw.pdf';
+import Button from '../components/Reusable/Button';
 
 const Hero = () => {
 
@@ -33,10 +34,12 @@ const Hero = () => {
             creating beautiful and functional websites. I am skilled in HTML,
             CSS, and JavaScript, and I am constantly learning.
           </p>
-          <div className="flex justify-center md:justify-start items-center flex-wrap gap-4 py-4">
-            <NavLink to="/contact" className="rounded-md bg-gradient-to-r from-brandBlue-500 to-brandBlue-600 hover:bg-brandBlue-600 active:bg-brandBlue-400 px-6 py-3 shadow-sm">Contact</NavLink>
+          <div className="flex justify-center md:justify-start items-center gap-4 py-4">
+            <NavLink to="/contact">
+              <Button>Contact</Button>
+            </NavLink>
 
-            <div className="flex items-center rounded-md dark:border dark:border-brandDark-700">
+            <div className="flex items-center rounded-xl dark:border dark:border-brandDark-700">
               <NavLink id="download-link"
                 onClick={() => {
                   const link = document.createElement('a');
@@ -44,26 +47,26 @@ const Hero = () => {
                   link.download = resumeLink.split('/').pop();
                   link.click();
                 }}
-                className="rounded-l-md bg-brandDark-800 px-6 py-3 space-x-2 shadow-md hover:bg-brandBlue-500">
-                Download Resume
+                className="rounded-l-xl bg-brandDark-800 px-6 py-3 space-x-2 shadow-md hover:bg-brandBlue-500">
+                View Resume
               </NavLink>
 
               <button id="dropdown-button" className="relative group">
                 <img src="./icons/arrow-down.svg"
                   alt="arrow-down"
-                  className="rounded-r-md bg-brandDark-800 px-2 py-3 h-full border-s-2 border-brandDark-700 hover:bg-brandBlue-500" />
+                  className="rounded-r-xl bg-brandDark-800 px-2 py-3 h-full border-s-2 border-brandDark-600 hover:bg-brandBlue-500" />
 
                 <div id="resume-colors"
-                  className="absolute hidden group-hover:block group-focus:block right-0 top-14 bg-brandDark-800 text-brandDark-100 rounded-md p-1.5 shadow-md space-y-1 border border-brandDark-700">
+                  className="absolute hidden group-hover:block group-focus:block right-0 top-14 z-50 bg-brandDark-800 text-brandDark-100 rounded-xl p-1.5 shadow-md border border-brandDark-700">
                   <p id="blue-option"
                     onClick={handleBlOption}
-                    className="hover:bg-brandDark-700 active:bg-brandBlue-500 rounded py-2 px-6 text-sm">Blue
+                    className="hover:bg-brandDark-700 active:bg-brandBlue-500 rounded-lg py-1.5 px-5 border border-transparent hover:border-brandDark-600 text-sm">Blue
                   </p>
 
                   <p id="bw-option"
                     title="Black & White"
                     onClick={handleBwOption}
-                    className="hover:bg-brandDark-700 active:bg-brandBlue-500 rounded py-2 px-6 text-sm">B&W</p>
+                    className="hover:bg-brandDark-700 active:bg-brandBlue-500 rounded-lg py-1.5 px-5 border border-transparent hover:border-brandDark-600 text-sm">B&W</p>
                 </div>
 
               </button>
